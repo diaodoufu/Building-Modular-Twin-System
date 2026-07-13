be7896e2340ac618bece977d23292c88a236f984
diaodoufu <1611682936@qq.com>
2026-07-13 15:38:46 +0800

    fix: remove unused _node variable

--- bmts-frontend/src/pages/Admin.vue
+++ bmts-frontend/src/pages/Admin.vue
@@ -41,7 +41,7 @@
           :default-expand-all="true"
           @node-click="onNodeClick"
         >
-          <template #default="{ _node, data }">
+          <template #default="{ data }">
             <span class="tree-node">
               <el-tag size="small" :type="typeTag(data.type)" style="margin-right: 4px">{{ typeLabel(data.type) }}</el-tag>
               <span>{{ data.name }}</span>
